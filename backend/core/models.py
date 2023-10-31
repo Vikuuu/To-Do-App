b6d9478b298832aa97a925dff0b7881fd7928dba
Aditya Vikram Singh <adivik672@gmail.com>
2023-10-31 22:09:30 +0530

    Set up for the Views in Core app of To-do Project

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Todolist(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateTimeField()
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+
+@api_view(["GET", "POST"])
+def getTodoList(request):
+    if request.method == "GET":
+        # Retrieve and return a list of all to-do items
+        pass
+
+    else:
+        # Create a new to-do item
+        pass
+
+
+@api_view(["GET", "PUT", "DELETE"])
+def getTodoTask(request, id):
+    if request.method == "GET":
+        # Retrieve and return a single to-do item by ID
+        pass
+
+    elif request.method == "POST":
+        # Update an existing to-do item by ID
+        pass
+
+    else:
+        # Delete a to-do item by ID
+        pass
+
+
+@api_view(["POST"])
+def completedTask(request):
+    # Mark a to-do item completed
+    pass

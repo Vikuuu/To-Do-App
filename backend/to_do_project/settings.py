588a4ef50f073ea9a3bb4cf3ee322ce83e8123e1
Aditya Vikram Singh <adivik672@gmail.com>
2023-10-31 19:14:50 +0530

    Added PostreSQL and the core app

--- backend/to_do_project/settings.py
+++ backend/to_do_project/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = "to_do_project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "to_do_app",
+        "USER": "postgres",
+        "PASSWORD": "jaibhawani09",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
